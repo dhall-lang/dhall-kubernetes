4a93f7b4f5c5349172c9e16d1faeb9f5d0decda9
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2018-10-31 23:53:56 +0200

    Upgrade dhall to 1.18.0 in nix config

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,10 +1,23 @@
 let
-  rev = "6b81005e66f1de9ccad34f2cf795fdb4f4f02942";
-  outputSha256 = "07dgrka8x1v5sh5wmq9698sdybjkx6qwb08pnsl9zgabww0i1s53";
+  rev = "19013d809297cb9dbba69bda24e52a2833f4e05a";
+  outputSha256 = "148nqqyb39xmxlnw4vgqin2s7ywq51yi64d2hqmd6pk2gqnhmpv9";
 
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     sha256 = outputSha256;
   };
+
+  config = {
+    packageOverrides = pkgs: rec {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: rec {
+          dhall = haskellPackagesNew.callPackage ./nix/dhall-1.18.0.nix {};
+          dhall-json = haskellPackagesNew.callPackage ./nix/dhall-json-1.2.4.nix {};
+          megaparsec = haskellPackagesNew.callPackage ./nix/megaparsec-7.0.2.nix {};
+          repline = haskellPackagesNew.callPackage ./nix/repline-0.2.0.0.nix {};
+        };
+      };
+    };
+  };
 in
-  import nixpkgs {}
+  import nixpkgs { inherit config; }
