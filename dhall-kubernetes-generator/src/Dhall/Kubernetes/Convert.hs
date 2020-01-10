ac4b0d1aeb4580a05a84609d19b490b80db08abf
Gabriel Gonzalez <Gabriel439@gmail.com>
2020-01-09 20:47:40 -0800

    Remove dependence on `kubernetes-client-core`

--- dhall-kubernetes-generator/src/Dhall/Kubernetes/Convert.hs
+++ dhall-kubernetes-generator/src/Dhall/Kubernetes/Convert.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE NamedFieldPuns   #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Dhall.Kubernetes.Convert
   ( toTypes
   , toDefault
@@ -6,6 +9,17 @@ module Dhall.Kubernetes.Convert
   , toDefinition
   ) where
 
+import Control.Applicative ((<|>))
+import Data.Aeson
+import Data.Aeson.Types (Parser, parseMaybe)
+import Data.Bifunctor (first, second)
+import Data.Maybe (fromMaybe, mapMaybe)
+import Data.Set (Set)
+import Data.Text (Text)
+import Dhall.Kubernetes.Types
+import GHC.Generics (Generic, Rep)
+
+import qualified Data.Char              as Char
 import qualified Data.List              as List
 import qualified Data.Map.Strict        as Data.Map
 import qualified Data.Set               as Set
@@ -13,18 +27,6 @@ import qualified Data.Sort              as Sort
 import qualified Data.Text              as Text
 import qualified Dhall.Core             as Dhall
 import qualified Dhall.Map
-
-import           Control.Applicative    ((<|>))
-import           Data.Aeson             
-import           Data.Aeson.Types       (parseMaybe)
-import           Data.Bifunctor         (first, second)
-import           Data.Maybe             (fromMaybe, mapMaybe)
-import           Data.Set               (Set)
-import           Data.Text              (Text)
-
-import           Dhall.Kubernetes.Types
-import           Kubernetes.OpenAPI.Model
-
 
 -- | Get all the required fields for a model
 --   See https://kubernetes.io/docs/concepts/overview/working-with-objects/kubernetes-objects/#required-fields
@@ -295,6 +297,86 @@ getImportsMap prefixMap duplicateNameHandler objectNames folder toInclude
         namespaced = case filter filterFn namespacedNames of
           [name] -> Just name
           names  -> duplicateNameHandler (kind, names)
+
+stripPrefix :: (Generic a, GFromJSON Zero (Rep a)) => Int -> Value -> Parser a
+stripPrefix n = genericParseJSON options
+  where
+    options = defaultOptions { fieldLabelModifier }
+
+    fieldLabelModifier string = case drop n string of
+        s : tring -> Char.toLower s : tring
+        []        -> []
+
+data V1beta1CustomResourceDefinition =
+    V1beta1CustomResourceDefinition
+        { v1beta1CustomResourceDefinitionSpec :: V1beta1CustomResourceDefinitionSpec
+        }
+    deriving (Generic)
+
+instance FromJSON V1beta1CustomResourceDefinition where
+    parseJSON = stripPrefix 31
+
+data V1beta1CustomResourceDefinitionSpec =
+    V1beta1CustomResourceDefinitionSpec
+        { v1beta1CustomResourceDefinitionSpecGroup :: Text
+        , v1beta1CustomResourceDefinitionSpecNames :: V1beta1CustomResourceDefinitionNames
+        , v1beta1CustomResourceDefinitionSpecValidation :: Maybe V1beta1CustomResourceValidation
+        , v1beta1CustomResourceDefinitionSpecVersion :: Maybe Text
+        , v1beta1CustomResourceDefinitionSpecVersions :: Maybe [V1beta1CustomResourceDefinitionVersion]
+        } deriving (Generic)
+
+instance FromJSON V1beta1CustomResourceDefinitionSpec where
+    parseJSON = stripPrefix 35
+
+data V1beta1CustomResourceDefinitionNames =
+    V1beta1CustomResourceDefinitionNames
+        { v1beta1CustomResourceDefinitionNamesKind :: Text
+        } deriving (Generic)
+
+instance FromJSON V1beta1CustomResourceDefinitionNames where
+    parseJSON = stripPrefix 36
+
+data V1beta1CustomResourceValidation =
+    V1beta1CustomResourceValidation
+        { v1beta1CustomResourceValidationOpenApiv3Schema :: Maybe V1beta1JSONSchemaProps
+        } deriving (Generic)
+
+instance FromJSON V1beta1CustomResourceValidation where
+    parseJSON = stripPrefix 31
+
+data V1beta1CustomResourceDefinitionVersion =
+    V1betaV1beta1CustomResourceDefinitionVersion
+        { v1beta1CustomResourceDefinitionVersionName :: Text
+        } deriving (Generic)
+
+instance FromJSON V1beta1CustomResourceDefinitionVersion where
+    parseJSON = stripPrefix 38
+
+data V1beta1JSONSchemaProps =
+    V1beta1JSONSchemaProps
+        { v1beta1JSONSchemaPropsRef :: Maybe Text
+        , v1beta1JSONSchemaPropsDescription :: Maybe Text
+        , v1beta1JSONSchemaPropsFormat :: Maybe Text
+        , v1beta1JSONSchemaPropsItems :: Maybe Value
+        , v1beta1JSONSchemaPropsProperties :: Maybe (Data.Map.Map String V1beta1JSONSchemaProps)
+        , v1beta1JSONSchemaPropsRequired :: Maybe [Text]
+        , v1beta1JSONSchemaPropsType :: Maybe Text
+        } deriving (Generic)
+
+instance FromJSON V1beta1JSONSchemaProps where
+    parseJSON = stripPrefix 22
+
+mkV1beta1JSONSchemaProps :: V1beta1JSONSchemaProps
+mkV1beta1JSONSchemaProps =
+    V1beta1JSONSchemaProps
+        { v1beta1JSONSchemaPropsRef = Nothing
+        , v1beta1JSONSchemaPropsDescription = Nothing
+        , v1beta1JSONSchemaPropsFormat = Nothing
+        , v1beta1JSONSchemaPropsItems = Nothing
+        , v1beta1JSONSchemaPropsProperties = Nothing
+        , v1beta1JSONSchemaPropsRequired = Nothing
+        , v1beta1JSONSchemaPropsType = Nothing
+        }
 
 toDefinition :: V1beta1CustomResourceDefinition -> Maybe (ModelName, Definition)
 toDefinition crd = 
